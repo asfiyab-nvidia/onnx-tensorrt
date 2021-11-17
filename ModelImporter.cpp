c15eadfcca5d7ebf29d379a2905d83bd961754de
William Raveane <wraveane@nvidia.com>
2021-11-17 15:17:07 -0500

    Mark MultilevelCropAndResize as a supported ORT-TRT contrib op

--- ModelImporter.cpp
+++ ModelImporter.cpp
@@ -427,7 +427,7 @@ bool ModelImporter::supportsOperator(const char* op_name) const
     {
         return false;
     }
-    if (std::string(op_name) == "EfficientNMS_TRT" || std::string(op_name) == "PyramidROIAlign_TRT")
+    if (std::string(op_name) == "EfficientNMS_TRT" || std::string(op_name) == "PyramidROIAlign_TRT" || std::string(op_name) == "MultilevelCropAndResize_TRT")
     {
         return true;
     }
